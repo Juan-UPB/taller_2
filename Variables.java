f7d3080096f85fc5f88fee95e743357a6ca7caa9
Juan <juanm.castro@upb.edu.co>
2026-08-18 11:34:14 -0500

    Agregar Variables.java

---
+++ Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte edad = 18;
+        short year = 2026;
+        long poblacion = 52000000L;
+
+        float altura = 1.80f;
+        double peso = 79.5;
+
+        boolean estudiante = true;
+        boolean trabaja = true;
+
+        char inicial = 'J';
+        char grupo = 'A';
+
+        String nombre = "Juan";
+        String ciudad = "Medellin";
+        String carrera = "Ingenieria de Sistemas";
+        String universidad = "UPB";
+
+        int materias = 6;
+        double promedio = 4.0;
+        float temperatura = 24.5f;
+
+        long distancia = 150000L;
+        byte edadHermano = 20;
+    }
+}
