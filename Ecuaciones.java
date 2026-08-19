1ea7d1d4101e828a7100f10bb24656eff223e67d
Juan <juanm.castro@upb.edu.co>
2026-08-18 23:08:16 -0500

    Agregar Ecuaciones.java

---
+++ Ecuaciones.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escoja una opcion:");
+        System.out.println("1. Ecuacion 1");
+        System.out.println("2. Ecuacion 2");
+        System.out.println("3. Salir");
+
+        int opcion = entrada.nextInt();
+
+        if (opcion == 3) {
+            System.out.println("Programa finalizado.");
+
+        } else if (opcion == 1 || opcion == 2) {
+
+            System.out.print("Digite el valor de x: ");
+            double x = entrada.nextDouble();
+
+            System.out.print("Digite el valor de z: ");
+            double z = entrada.nextDouble();
+
+            double y;
+
+            if (opcion == 1) {
+
+                y = ((3 * x) / (1 + (3 * x)))
+                        * ((1.0 / (1 + z)) + (3 * x * x) + (2 * z) + 3)
+                        / ((3 * z * z) + 2);
+
+                System.out.println("El resultado de y es: " + y);
+
+            } else if (opcion == 2) {
+
+                y = ((x * x) + (3 * z) + 2)
+                        / (2 + (1.0 / (1 + (2 * z))))
+                        + 2.0 / (
+                            (1 + (3 * x * x * x) + (3 * z) + 2)
+                            / (
+                                (1.0 / (1 + (3 * x)))
+                                + ((3 * x + 1)
+                                / ((2 * z) + (3.0 / (1 + (5.0 / z)))))
+                            )
+                        );
+
+                System.out.println("El resultado de y es: " + y);
+            }
+
+        } else {
+            System.out.println("Opcion no valida.");
+        }
+
+        entrada.close();
+    }
+}
